62864c5f990b55782e791646ddc6aa88fc9709a4
Parth Ahuja <parthahuja.in@gmail.com>
2024-06-08 13:54:15 +0530

    Todo App Created

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Todo>> GetTodos()
+        {
+            return Todo.Todos;  // This will include the dummy todos
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Todo> GetTodo(int id)
+        {
+            var todo = Todo.Todos.Find(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return todo;
+        }
+
+        [HttpPost]
+        public ActionResult<Todo> CreateTodo(Todo todo)
+        {
+            todo.IsComplete = false;
+            Todo.Todos.Add(todo);
+            return CreatedAtAction(nameof(GetTodo), new { id = todo.Id }, todo);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTodo(int id, Todo todo)
+        {
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingTodo = Todo.Todos.Find(t => t.Id == id);
+            if (existingTodo == null)
+            {
+                return NotFound();
+            }
+
+            existingTodo.Title = todo.Title;
+            existingTodo.IsComplete = todo.IsComplete;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodo(int id)
+        {
+            var todo = Todo.Todos.Find(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            Todo.Todos.Remove(todo);
+            return NoContent();
+        }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace TodoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+
+        public static List<Todo> Todos { get; } = new List<Todo>();
+        public static int NextId { get; set; } = 1;
+
+        public Todo()
+        {
+            Id = NextId++;
+        }
+
+        // Static constructor to add dummy todos
+        static Todo()
+        {
+            Todos.Add(new Todo { Title = "Learn ASP.NET Core", IsComplete = false });
+            Todos.Add(new Todo { Title = "Build a Web API", IsComplete = true });
+            Todos.Add(new Todo { Title = "Test API with Postman", IsComplete = false });
+            Todos.Add(new Todo { Title = "Deploy to Azure", IsComplete = false });
+            Todos.Add(new Todo { Title = "Write API documentation", IsComplete = false });
+        }
+    }
+}
